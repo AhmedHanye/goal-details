c1d440b1b915746f63d7ef71a575db0c2acd752e
ahmedhanye <ahmedhanyehossny@gmail.com>
2025-03-10 14:44:23 +0200

    add TargetDescription component and Textarea UI component

--- src/components/GoalDetails/target.tsx
+++ src/components/GoalDetails/target.tsx
@@ -1,13 +1,15 @@
+import TargetDescription from "./target/targetDescription";
 import TargetProgress from "./target/targetProgress";
 import TargetUpdate from "./target/targetUpdate";
 import Uaggregators from "./target/uaggregators";
 
 const Target = () => {
   return (
-    <div className="mx-auto flex h-screen w-[42.5rem] flex-col gap-y-8 px-4 py-16">
+    <div className="mx-auto flex w-[42.5rem] flex-col gap-y-8 px-4 py-16">
       <TargetUpdate />
       <Uaggregators />
       <TargetProgress />
+      <TargetDescription />
     </div>
   );
 };
